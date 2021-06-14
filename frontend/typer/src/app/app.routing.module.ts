16de1e7133e5a9323e73db50229e594cb869e8cb
iwan <iwan127@gmail.com>
2021-06-14 23:11:04 +0200

    typy jako domyślna strona

--- frontend/typer/src/app/app.routing.module.ts
+++ frontend/typer/src/app/app.routing.module.ts
@@ -24,7 +24,7 @@ const routes: Routes = [
     children: [
       {
         path: '',
-        redirectTo: 'matches',
+        redirectTo: 'predictions',
         pathMatch: 'full'
       },
       {
